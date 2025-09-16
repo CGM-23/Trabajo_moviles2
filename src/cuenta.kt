bb5347367660f5985353d933cc678710d3e041ee
JhairRoussell2 <jhairrmb3@gmail.com>
2025-09-15 23:59:44 -0500

    Creación de la clase padre,la principal del cual heredarán el resto de las clases

---
+++ src/cuenta.kt
@@ -0,0 +1,42 @@
+// --------------------
+// Clase base Cuenta
+open class Cuenta(
+    protected var saldo: Float,
+    protected var tasaAnual: Float
+) {
+    protected var numeroConsignaciones: Int = 0
+    protected var numeroRetiros: Int = 0
+    protected var comisionMensual: Float = 0f
+
+    open fun consignar(cantidad: Float) {
+        saldo += cantidad
+        numeroConsignaciones++
+    }
+
+    open fun retirar(cantidad: Float) {
+        if (cantidad <= saldo) {
+            saldo -= cantidad
+            numeroRetiros++
+        } else {
+            println(" No se puede retirar: saldo insuficiente")
+        }
+    }
+
+    protected fun calcularInteresMensual() {
+        val interesMensual = (tasaAnual / 12) * saldo / 100
+        saldo += interesMensual
+    }
+
+    open fun extractoMensual() {
+        saldo -= comisionMensual
+        calcularInteresMensual()
+    }
+
+    open fun imprimir() {
+        println("Saldo: $saldo")
+        println("Número de consignaciones: $numeroConsignaciones")
+        println("Número de retiros: $numeroRetiros")
+        println("Tasa anual: $tasaAnual%")
+        println("Comisión mensual: $comisionMensual")
+    }
+}
